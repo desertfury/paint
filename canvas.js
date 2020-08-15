4d641e9e8b86651909a57fd6204aa84b0f1bc3ec
Mikhail Chichvarin <desertfury@gmail.com>
2020-08-16 00:31:14 +0300

    keydown instead of dblclick

--- canvas.js
+++ canvas.js
@@ -1,8 +1,6 @@
 window.addEventListener("load", (ev) => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
-    ctx.lineWidth = 1;
-    ctx.lineCap = "round";
 
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
@@ -25,13 +23,12 @@ window.addEventListener("load", (ev) => {
             return;
         }
         points.slice(-1)[0].push(ev);
+        ctx.lineWidth = 10;
+        ctx.lineCap = "round";
         ctx.lineTo(ev.clientX, ev.clientY);
         ctx.stroke();
     };
     const undo = () => {
-        while (points.length && points.slice(-1)[0].length == 1) {
-            points.pop();
-        }
         points.pop();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (const lines of points) {
@@ -46,5 +43,7 @@ window.addEventListener("load", (ev) => {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", endPainting);
     canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener('dblclick', undo);//TODO: figure out how to distinguish mousedown from doubleclick
+    canvas.addEventListener('keydown',(e) => {
+        undo();
+    });
 });
