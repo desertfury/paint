e74ee813a06cf1c6fe3c0248de9638a4284f0d61
Mikhail Chichvarin <desertfury@gmail.com>
2020-08-16 00:11:26 +0300

    undo on double click

--- canvas.js
+++ canvas.js
@@ -1,15 +1,18 @@
 window.addEventListener("load", (ev) => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
+    ctx.lineWidth = 1;
+    ctx.lineCap = "round";
 
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
     let painting = false;
+    let points = [];
 
     const startPainting = (ev) => {
         painting = true;
-        moves.push([]);
+        points.push([]);
         ctx.beginPath();
         ctx.moveTo(ev.clientX, ev.clientY);
         draw(ev);
@@ -21,13 +24,27 @@ window.addEventListener("load", (ev) => {
         if (!painting) {
             return;
         }
-        ctx.lineWidth = 1;
-        ctx.lineCap = "round";
+        points.slice(-1)[0].push(ev);
         ctx.lineTo(ev.clientX, ev.clientY);
         ctx.stroke();
     };
+    const undo = () => {
+        while (points.length && points.slice(-1)[0].length == 1) {
+            points.pop();
+        }
+        points.pop();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (const lines of points) {
+            ctx.beginPath();
+            for (const ev of lines) {
+                ctx.lineTo(ev.clientX, ev.clientY);
+                ctx.stroke();
+            }
+        }
+    }
 
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", endPainting);
-    canvas.addEventListener("mousemove", draw); 
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener('dblclick', undo);//TODO: figure out how to distinguish mousedown from doubleclick
 });
